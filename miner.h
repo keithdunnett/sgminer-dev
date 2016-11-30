3139bb2139d58f5bbe3b541a059becee96fe1d24
magick777 <keith@dunnett.name>
2016-11-30 03:18:55 +0000

    Use Khronos OpenCL headers, v1.2 as default as that's what many vendors are still using.

--- miner.h
+++ miner.h
@@ -24,10 +24,10 @@ extern char *curly;
 #endif
 #include <sched.h>
 
-#if defined(USE_GIT_VERSION) && defined(GIT_VERSION)
-#undef VERSION
-#define VERSION GIT_VERSION
-#endif
+//#if defined(USE_GIT_VERSION) && defined(GIT_VERSION)
+//#undef VERSION
+//#define VERSION GIT_VERSION
+//#endif
 
 #ifdef BUILD_NUMBER
 #define CGMINER_VERSION VERSION "-" BUILD_NUMBER
