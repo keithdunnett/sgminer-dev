884a7481a639eb2202b828785da04f3df84c211a
magick777 <keith@dunnett.name>
2016-11-30 01:23:46 +0000

    Remove yescrypt. Not too useful for GPU mining, and had a few bugs.

--- ocl.c
+++ ocl.c
@@ -36,7 +36,7 @@
 #include "ocl/binary_kernel.h"
 #include "algorithm/neoscrypt.h"
 #include "algorithm/pluck.h"
-#include "algorithm/yescrypt.h"
+//#include "algorithm/yescrypt.h"
 #include "algorithm/lyra2rev2.h"
 
 /* FIXME: only here for global config vars, replace with configuration.h
@@ -541,89 +541,88 @@ _clState *initCl(unsigned int gpu, char *name, size_t nameSize, algorithm_t *alg
   }
 
   // Yescrypt TC
-  else if ((cgpu->algorithm.type == ALGO_YESCRYPT ||
-            algorithm->type == ALGO_YESCRYPT_MULTI) && !cgpu->opt_tc) {
-    size_t glob_thread_count;
-    long max_int;
-    unsigned char type = 0;
-
-      // determine which intensity type to use
-    if (cgpu->rawintensity > 0) {
-      glob_thread_count = cgpu->rawintensity;
-      max_int = glob_thread_count;
-      type = 2;
-    }
-    else if (cgpu->xintensity > 0) {
-      glob_thread_count = clState->compute_shaders * ((cgpu->algorithm.xintensity_shift) ? (1UL << (cgpu->algorithm.xintensity_shift + cgpu->xintensity)) : cgpu->xintensity);
-      max_int = cgpu->xintensity;
-      type = 1;
-    }
-    else {
-      glob_thread_count = 1UL << (cgpu->algorithm.intensity_shift + cgpu->intensity);
-      max_int = ((cgpu->dynamic) ? MAX_INTENSITY : cgpu->intensity);
-    }
-
-    glob_thread_count = ((glob_thread_count < cgpu->work_size) ? cgpu->work_size : glob_thread_count);
-
-    // if TC * scratchbuf size is too big for memory... reduce to max
-    if ((glob_thread_count * YESCRYPT_SCRATCHBUF_SIZE) >= (uint64_t)cgpu->max_alloc) {
-
-      /* Selected intensity will not run on this GPU. Not enough memory.
-      * Adapt the memory setting. */
-      // depending on intensity type used, reduce the intensity until it fits into the GPU max_alloc
-      switch (type) {
-        //raw intensity
-      case 2:
-        while ((glob_thread_count * YESCRYPT_SCRATCHBUF_SIZE) > (uint64_t)cgpu->max_alloc) {
-          --glob_thread_count;
-        }
-
-        max_int = glob_thread_count;
-        cgpu->rawintensity = glob_thread_count;
-        break;
-
-        //x intensity
-      case 1:
-        glob_thread_count = cgpu->max_alloc / YESCRYPT_SCRATCHBUF_SIZE;
-        max_int = glob_thread_count / clState->compute_shaders;
-
-        while (max_int && ((clState->compute_shaders * (1UL << max_int)) > glob_thread_count)) {
-          --max_int;
-        }
-
-        /* Check if max_intensity is >0. */
-        if (max_int < MIN_XINTENSITY) {
-          applog(LOG_ERR, "GPU %d: Max xintensity is below minimum.", gpu);
-          max_int = MIN_XINTENSITY;
-        }
-
-        cgpu->xintensity = max_int;
-        glob_thread_count = clState->compute_shaders * (1UL << max_int);
-        break;
-
-      default:
-        glob_thread_count = cgpu->max_alloc / YESCRYPT_SCRATCHBUF_SIZE;
-        while (max_int && ((1UL << max_int) & glob_thread_count) == 0) {
-          --max_int;
-        }
-
-        /* Check if max_intensity is >0. */
-        if (max_int < MIN_INTENSITY) {
-          applog(LOG_ERR, "GPU %d: Max intensity is below minimum.", gpu);
-          max_int = MIN_INTENSITY;
-        }
-
-        cgpu->intensity = max_int;
-        glob_thread_count = 1UL << max_int;
-        break;
-      }
-    }
-
-    // TC is glob thread count
-    cgpu->thread_concurrency = glob_thread_count;
-
-    applog(LOG_DEBUG, "GPU %d: computing max. global thread count to %u", gpu, (unsigned)(cgpu->thread_concurrency));
-  }
+//  else if ((cgpu->algorithm.type == ALGO_YESCRYPT ||
+//            algorithm->type == ALGO_YESCRYPT_MULTI) && !cgpu->opt_tc) {
+//    size_t glob_thread_count;
+//    long max_int;
+//    unsigned char type = 0;
+//
+//      // determine which intensity type to use
+//    if (cgpu->rawintensity > 0) {
+//      glob_thread_count = cgpu->rawintensity;
+//      max_int = glob_thread_count;
+//      type = 2;
+//    }
+//    else if (cgpu->xintensity > 0) {
+//      glob_thread_count = clState->compute_shaders * ((cgpu->algorithm.xintensity_shift) ? (1UL << (cgpu->algorithm.xintensity_shift + cgpu->xintensity)) : cgpu->xintensity);
+//      max_int = cgpu->xintensity;
+//      type = 1;
+//    }
+//    else {
+//      glob_thread_count = 1UL << (cgpu->algorithm.intensity_shift + cgpu->intensity);
+//      max_int = ((cgpu->dynamic) ? MAX_INTENSITY : cgpu->intensity);
+//    }
+//
+//    glob_thread_count = ((glob_thread_count < cgpu->work_size) ? cgpu->work_size : glob_thread_count);
+//
+//    // if TC * scratchbuf size is too big for memory... reduce to max
+//    if ((glob_thread_count * YESCRYPT_SCRATCHBUF_SIZE) >= (uint64_t)cgpu->max_alloc) {
+//
+//      /* Selected intensity will not run on this GPU. Not enough memory.
+//      * Adapt the memory setting. */
+//      // depending on intensity type used, reduce the intensity until it fits into the GPU max_alloc
+//      switch (type) {
+//        //raw intensity
+//      case 2:
+//        while ((glob_thread_count * YESCRYPT_SCRATCHBUF_SIZE) > (uint64_t)cgpu->max_alloc) {
+//          --glob_thread_count;
+//        }
+//
+//        max_int = glob_thread_count;
+//        cgpu->rawintensity = glob_thread_count;
+//        break;
+//
+//        //x intensity
+//      case 1:
+//        glob_thread_count = cgpu->max_alloc / YESCRYPT_SCRATCHBUF_SIZE;
+//        max_int = glob_thread_count / clState->compute_shaders;
+//
+//        while (max_int && ((clState->compute_shaders * (1UL << max_int)) > glob_thread_count)) {
+//          --max_int;
+//        }
+//      /* Check if max_intensity is >0. */
+//       if (max_int < MIN_XINTENSITY) {
+//          applog(LOG_ERR, "GPU %d: Max xintensity is below minimum.", gpu);
+//          max_int = MIN_XINTENSITY;
+//        }
+//
+//        cgpu->xintensity = max_int;
+//        glob_thread_count = clState->compute_shaders * (1UL << max_int);
+//        break;
+//
+//      default:
+//        glob_thread_count = cgpu->max_alloc / YESCRYPT_SCRATCHBUF_SIZE;
+//        while (max_int && ((1UL << max_int) & glob_thread_count) == 0) {
+//          --max_int;
+//        }
+//
+//        /* Check if max_intensity is >0. */
+//        if (max_int < MIN_INTENSITY) {
+//         applog(LOG_ERR, "GPU %d: Max intensity is below minimum.", gpu);
+//          max_int = MIN_INTENSITY;
+//        }
+//
+//        cgpu->intensity = max_int;
+//        glob_thread_count = 1UL << max_int;
+//        break;
+//      }
+//    }
+//
+//    // TC is glob thread count
+//    cgpu->thread_concurrency = glob_thread_count;
+//
+//    applog(LOG_DEBUG, "GPU %d: computing max. global thread count to %u", gpu, (unsigned)(cgpu->thread_concurrency));
+//  }
 
   // Lyra2re v2 TC
   else if (cgpu->algorithm.type == ALGO_LYRA2REV2 && !cgpu->opt_tc) {
@@ -838,19 +837,19 @@ _clState *initCl(unsigned int gpu, char *name, size_t nameSize, algorithm_t *alg
       applog(LOG_DEBUG, "pluck buffer sizes: %lu RW, %lu R", (unsigned long)bufsize, (unsigned long)readbufsize);
       // scrypt/n-scrypt
     }
-    else if (algorithm->type == ALGO_YESCRYPT || algorithm->type == ALGO_YESCRYPT_MULTI) {
-      /* The scratch/pad-buffer needs 32kBytes memory per thread. */
-      bufsize = YESCRYPT_SCRATCHBUF_SIZE * cgpu->thread_concurrency;
-      buf1size = PLUCK_SECBUF_SIZE * cgpu->thread_concurrency;
-      buf2size = 128 * 8 * 8 * cgpu->thread_concurrency;
-      buf3size= 8 * 8 * 4 * cgpu->thread_concurrency;
-      /* This is the input buffer. For yescrypt this is guaranteed to be
-      * 80 bytes only. */
-      readbufsize = 80;
+//    else if (algorithm->type == ALGO_YESCRYPT || algorithm->type == ALGO_YESCRYPT_MULTI) {
+//      /* The scratch/pad-buffer needs 32kBytes memory per thread. */
+//      bufsize = YESCRYPT_SCRATCHBUF_SIZE * cgpu->thread_concurrency;
+//      buf1size = PLUCK_SECBUF_SIZE * cgpu->thread_concurrency;
+//      buf2size = 128 * 8 * 8 * cgpu->thread_concurrency;
+//      buf3size= 8 * 8 * 4 * cgpu->thread_concurrency;
+//      /* This is the input buffer. For yescrypt this is guaranteed to be
+//      * 80 bytes only. */
+//      readbufsize = 80;
 
-      applog(LOG_DEBUG, "yescrypt buffer sizes: %lu RW, %lu R", (unsigned long)bufsize, (unsigned long)readbufsize);
-      // scrypt/n-scrypt
-    }
+//      applog(LOG_DEBUG, "yescrypt buffer sizes: %lu RW, %lu R", (unsigned long)bufsize, (unsigned long)readbufsize);
+ //     // scrypt/n-scrypt
+//    }
     else if (algorithm->type == ALGO_LYRA2REV2) {
       /* The scratch/pad-buffer needs 32kBytes memory per thread. */
       bufsize = LYRA_SCRATCHBUF_SIZE * cgpu->thread_concurrency;
@@ -889,26 +888,26 @@ _clState *initCl(unsigned int gpu, char *name, size_t nameSize, algorithm_t *alg
       applog(LOG_WARNING, "Your settings come to %lu", (unsigned long)bufsize);
     }
 
-    if (algorithm->type == ALGO_YESCRYPT || algorithm->type == ALGO_YESCRYPT_MULTI) {
-      // need additionnal buffers
-      clState->buffer1 = clCreateBuffer(clState->context, CL_MEM_READ_WRITE, buf1size, NULL, &status);
-      if (status != CL_SUCCESS && !clState->buffer1) {
-        applog(LOG_DEBUG, "Error %d: clCreateBuffer (buffer1), decrease TC or increase LG", status);
-        return NULL;
-      }
-
-      clState->buffer2 = clCreateBuffer(clState->context, CL_MEM_READ_WRITE, buf2size, NULL, &status);
-      if (status != CL_SUCCESS && !clState->buffer2) {
-        applog(LOG_DEBUG, "Error %d: clCreateBuffer (buffer2), decrease TC or increase LG", status);
-        return NULL;
-      }
-
-      clState->buffer3 = clCreateBuffer(clState->context, CL_MEM_READ_WRITE, buf3size, NULL, &status);
-      if (status != CL_SUCCESS && !clState->buffer3) {
-        applog(LOG_DEBUG, "Error %d: clCreateBuffer (buffer3), decrease TC or increase LG", status);
-        return NULL;
-      }
-    }
+//    if (algorithm->type == ALGO_YESCRYPT || algorithm->type == ALGO_YESCRYPT_MULTI) {
+//      // need additionnal buffers
+//      clState->buffer1 = clCreateBuffer(clState->context, CL_MEM_READ_WRITE, buf1size, NULL, &status);
+//      if (status != CL_SUCCESS && !clState->buffer1) {
+//        applog(LOG_DEBUG, "Error %d: clCreateBuffer (buffer1), decrease TC or increase LG", status);
+//        return NULL;
+//      }
+//
+//      clState->buffer2 = clCreateBuffer(clState->context, CL_MEM_READ_WRITE, buf2size, NULL, &status);
+//      if (status != CL_SUCCESS && !clState->buffer2) {
+//        applog(LOG_DEBUG, "Error %d: clCreateBuffer (buffer2), decrease TC or increase LG", status);
+//        return NULL;
+//      }
+//
+//      clState->buffer3 = clCreateBuffer(clState->context, CL_MEM_READ_WRITE, buf3size, NULL, &status);
+//      if (status != CL_SUCCESS && !clState->buffer3) {
+//        applog(LOG_DEBUG, "Error %d: clCreateBuffer (buffer3), decrease TC or increase LG", status);
+//        return NULL;
+//      }
+//    }
     else if (algorithm->type == ALGO_LYRA2REV2) {
       // need additionnal buffers
       clState->buffer1 = clCreateBuffer(clState->context, CL_MEM_READ_WRITE, buf1size, NULL, &status);
