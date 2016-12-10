38a66a02c410ef2c0494e392aeb0bda7a7fc4cfc
magick777 <keith@dunnett.name>
2016-12-10 17:50:24 +0000

    Adds FindXXX.cmake modules, update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 
+rm -rf CMakeFiles CMakeCache.txt
+
 CMAKE_ARGS="$1"
 CLEAN_FIRST=""
 
 threads=$((`cat /proc/cpuinfo | grep processor | wc -l` + 1)); 
 
 cmake "$CMAKE_ARGS" .
-
 cmake --build . --clean-first -- -j$threads 
+
+#scan-build-3.8 make -j$threads 
